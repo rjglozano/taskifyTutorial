07a7a7962d9437e0a984101e7de16185d2053960
ryanlozano07 <ryan.rjl38@gmail.com>
2024-02-11 02:08:27 +0800

    react & typescript

---
+++ src/components/InputField.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './styles.css';
+
+interface Props{
+    todo:string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField:React.FC<Props> = ({todo, setTodo, handleAdd}) => {
+  return (
+    <form className='input' onSubmit={handleAdd}>
+        <input 
+            type='input' 
+            placeholder='Enter a task' 
+            className='input__box' 
+            value={todo}
+            onChange={(e)=>setTodo(e.target.value)}
+        />
+        <button 
+            className='input_submit' 
+            type='submit'
+        >
+            Go
+        </button>
+    </form>
+  )
+}
+
+export default InputField
