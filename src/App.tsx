abb78bb99b264394a780f91ab56c55ff9c902e74
ryanlozano07 <ryan.rjl38@gmail.com>
2024-02-12 19:57:35 +0800

    draggable and droppable list

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 import InputField from './components/InputField';
 import { Todo } from './model';
 import TodoList from './components/TodoList';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd'
 
 const App:React.FC = () => {
 
   const [todo, setTodo] = useState<string>("")
   const [todos, setTodos] = useState<Todo[]>([])
+  const [completedTodos, setcompletedTodos] = useState<Todo[]>([])
 
   const handleAdd = (e: React.FormEvent) =>{
     e.preventDefault();
@@ -18,16 +20,55 @@ const App:React.FC = () => {
     }
   }
 
-  console.log(todos)
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
+
+    console.log(result);
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    let add;
+    let active = todos;
+    let complete = completedTodos;
+
+    if (source.droppableId === "TodosList") {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+    if (destination.droppableId === "TodosList") {
+      active.splice(destination.index, 0, add);
+    } else {
+      complete.splice(destination.index, 0, add);
+    }
+
+    setcompletedTodos(complete);
+    setTodos(active);
+  };
 
 
   return (
+    <DragDropContext onDragEnd={(onDragEnd)}>
+
     <div className='App'>
       <span className="heading">Tasktify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-      <TodoList todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} setTodos={setTodos} completedTodos={completedTodos} setcompletedTodos={setcompletedTodos} />
    
     </div>
+    </DragDropContext>
+   
     
   );
 }
--- src/model.ts
+++ src/model.ts
@@ -3,3 +3,23 @@ export interface Todo{
     todo: string, 
     isDone: boolean
 }
+
+// type Actions = 
+//     | { type: 'add', payload: string}
+//     | { type: 'remove', payload: number}
+//     | { type: 'done', payload: number}
+
+
+
+// const TodoReducer = (state:Todo[], action:Actions)=>{
+
+// }
+
+// import { useReducer } from "react"
+// const ReducerExample = () =>{
+//     const [state, dispatch] = useReducer(TodoReducer, [])
+
+//     return(
+     
+//     )
+// }
